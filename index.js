679d39222ba228789975db0d5043e71d10cc1b20
donatasadamonis <dadamonis@gsma.com>
2019-06-05 08:59:01 +0100

    multistreamer

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const path = require('path');
 const express = require('express');
-const http = require('http');
+const http = require('https');
 const WebSocket = require('ws');
 const fs = require('fs');
 const app = express();
                 //SSLCertificateFile      /etc/ssl/certs/apache-selfsigned.crt
                 //SSLCertificateKeyFile /etc/ssl/private/apache-selfsigned.key
-//var key = fs.readFileSync( '/etc/ssl/private/apache-selfsigned.key');
-//var cert = fs.readFileSync( '/etc/ssl/certs/apache-selfsigned.crt');
-//var options = {
-//  key: key,
-//  cert: cert
-//};
-//const httpServer = http.createServer(options, app);
-const httpServer = http.createServer( app);
+var key = fs.readFileSync( '/etc/ssl/private/apache-selfsigned.key');
+var cert = fs.readFileSync( '/etc/ssl/certs/apache-selfsigned.crt');
+var options = {
+  key: key,
+  cert: cert
+};
+const httpServer = http.createServer(options, app);
+//const httpServer = http.createServer( app);
 
 const PORT = process.env.PORT || 3003;
 
@@ -24,16 +24,47 @@ let connectedClients = [];
 
 wsServer.on('connection', (ws, req) => {
     console.log('Connected');
+	ws.markedAsSender = false;
     // add new connected client
     connectedClients.push(ws);
     // listen for messages from the streamer, the clients will not send anything so we don't need to filter
     ws.on('message', data => {
+		let streamerNo = JSON.parse(data).streamerNo;
+		if(!ws.markedAsSender){
+			console.log('marking as sender as msg received');
+			ws.markedAsSender = true;
+			ws.streamerNo = streamerNo;
+		}
+		//console.log("message incomming",connectedClients);
         // send the base64 encoded frame to each connected ws
-        connectedClients.forEach((ws, i) => {
-            if (ws.readyState === ws.OPEN) { // check if it is still connected
-                ws.send(data); // send
+        connectedClients.forEach((ws_i, i) => {
+            if (ws_i.readyState === ws_i.OPEN) { // check if it is still connected
+				if(ws_i == ws || ws_i.markedAsSender){
+					//console.log('same as sender not sending img back', i);
+					
+					//return 0;
+				}
+				else{
+					//console.log('not same as sender sending img', i);
+                	ws_i.send(data); // send
+				}
             } else { // if it's not connected remove from the array of connected ws
-                connectedClients.splice(i, 1);
+                if(connectedClients[i].markedAsSender){
+					console.log('need to send receivers closed streamer notice');
+					connectedClients.forEach((ws_c, ii) => {   
+						if(ws_c.markedAsSender){
+						
+						}else{
+							console.log('sending to client no ', ws_i.streamerNo);
+							let response = { data:"disconnected", streamerNo: ws_i.streamerNo };
+							ws_c.send(JSON.stringify(response));
+						}
+					})
+				}
+				connectedClients.splice(i, 1);
+				console.log('disconnected client ',i);
+				
+
             }
         });
     });
